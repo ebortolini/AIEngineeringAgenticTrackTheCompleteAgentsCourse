f9c9e9865c334f3895ea9ecd125520ede586ee10
Eduardo Bortolini <ebortolini@gmail.com>
2026-03-01 22:11:12 -0300

    [Week 2] Day 5 - Building a Modular AI Research System

---
+++ 2_openai/deep_research/email_agent.py
@@ -0,0 +1,22 @@
+import os
+from typing import Dict
+from agents import Agent, function_tool
+
+
+@function_tool
+def send_email(subject: str, html_body: str) -> Dict[str, str]:
+    """Send an email with the given subject and HTML body"""
+    print(f"Sending email with subject: {subject} and content:\n{html_body}")
+    return "success"
+
+
+INSTRUCTIONS = """You are able to send a nicely formatted HTML email based on a detailed report.
+You will be provided with a detailed report. You should use your tool to send one email, providing the 
+report converted into clean, well presented HTML with an appropriate subject line."""
+
+email_agent = Agent(
+    name="Email agent",
+    instructions=INSTRUCTIONS,
+    tools=[send_email],
+    model="gpt-4o-mini",
+)
